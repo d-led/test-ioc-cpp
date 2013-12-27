6fd252ae6387fcadf2a2152d47bf7ec5910313e8
Dmitry Ledentsov <dmlled@yahoo.com>
2013-12-27 16:46:39 +0100

    hypodermic doesn't build on mac yet, so, no test

---
+++ test_hypodermic.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "common/interfaces/irender.h"
+#include "common/interfaces/imodel.h"
+#include "common/interfaces/igetkeyvalue.h"
+
+#include <Hypodermic/ContainerBuilder.h>
+
+using Hypodermic::ContainerBuilder;
+
+TEST(hypodermic_hello_world,example) {
+	ContainerBuilder builder;
+
+	// inject_renderer(builder);
+	// inject_decoder(builder);
+
+	// auto container = builder.build();
+
+	// auto renderer = container->resolve< IRender >();
+
+	// ASSERT_EQ( "a,b" , renderer->Render() );
+}
