32623140e6bae20df9fbd236ae9faf194ec9a572
Dmitry Ledentsov <dmlled@yahoo.com>
2013-12-28 22:27:10 +0100

    sauce test ok, mock not configurable

--- test_sauce.cpp
+++ test_sauce.cpp
@@ -6,12 +6,9 @@
 #include <sauce/sauce.h>
 
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
 
 using sauce::Modules;
 using sauce::Injector;
-using ::testing::AtLeast;
-using ::testing::Return;
 
 namespace {
 
@@ -19,33 +16,26 @@ namespace {
 
 	class MockModel : public IModel {
 	public:
-		MOCK_METHOD0(Get, std::string());
+		virtual std::string Get() {
+			return "{ \"a\" : 1 , \"b\" : 2 }";
+		}
 	};
 
 	void model_module(sauce::Binder& b) {
-		b.bind<IModel>().in<SingletonScope>().to<MockModel>();
+		b.bind<IModel>().to<MockModel>();
 	}
 }
 
 TEST(sauce_hello_world,all_together) {
 
-	Modules modules;
+	sauce::shared_ptr<Injector> injector = Modules()
+		.add(render_module)
+		.add(decoder_module)
+		.add(model_module)
+		.createInjector()
+	;
 
-	modules.add(render_module);
-	modules.add(decoder_module);
-	modules.add(model_module);
-
-	// prepare injector
-	sauce::shared_ptr<Injector> injector = modules.createInjector();
-
-	// prepare mock
-	sauce::shared_ptr<MockModel> mock_model = std::static_pointer_cast<MockModel>(injector->get<IModel>());
-	ASSERT_TRUE( mock_model.get() );
-
-	ASSERT_TRUE( mock_model.get() );
-	EXPECT_CALL(*mock_model, Get())
-		.Times(AtLeast(1))
-		.WillRepeatedly(Return("{ \"a\" : 1 , \"b\" : 2 }"));
+	injector->enter<SingletonScope>();
 
 	// renderer instance
 	sauce::shared_ptr<IRender> renderer = injector->get<IRender>();
